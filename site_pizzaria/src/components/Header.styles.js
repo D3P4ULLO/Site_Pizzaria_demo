a898b7f2a5e31fbe7eb7572e3802f2ab1ffedb7d
D3P4ULLO <matheus_depaulo@hotmail.com>
2026-03-19 11:46:51 -0300

    aletação e ajuste da barra de navegação

--- site_pizzaria/src/components/Header.styles.js
+++ site_pizzaria/src/components/Header.styles.js
@@ -2,15 +2,18 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  background: #fa6a62;
+  background: #fa6868;
   color: white;
-  padding: 16px 0px;
+  padding: 20px 30px;
+  border-bottom: 4px solid #99363f;
+  border-radius: 2px 2px 20px 20px;
 `;
 
 export const Content = styled.div`
   max-width: 1200px;
+
   margin: auto;
-  padding: 0 20px;
+  padding: 0 2rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -21,18 +24,19 @@ export const Logo = styled(Link)`
   font-weight: bold;
   font-style: italic;
   text-decoration: none;
-  color: white;
-  margin-left: 0px;
+  color: whitesmoke;
+  margin-left: 1rem;
 `;
 
 export const Nav = styled.nav`
   display: flex;
-  gap: 20px;
+  gap: 3rem;
+  margin-right: 1.4rem;
 `;
 
 export const NavLink = styled(Link)`
   text-decoration: none;
-  color: white;
+  color: whitesm;
   font-weight: 500;
 
   position: relative;
